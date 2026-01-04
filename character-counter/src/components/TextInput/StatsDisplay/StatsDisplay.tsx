8fc6b8a1f24f62906df406e6e27b0306ac4c8d37
thiernohbah <thiernobah2001@gmail.com>
2026-01-04 16:19:48 -0500

    ive made changes to my statsdisplay so that it can add numbers revive stars as a prop

---
+++ character-counter/src/components/TextInput/StatsDisplay/StatsDisplay.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StatsDisplayProps } from '../../types';
+
+export const StatsDisplay: React.FC<StatsDisplayProps> = ({ stats }) => {
+  return (
+    <div className="flex gap-4 mt-4">
+      <div>
+        <p>Characters</p>
+        <strong>{stats.characterCount}</strong>
+      </div>
+
+      <div>
+        <p>Words</p>
+        <strong>{stats.wordCount}</strong>
+      </div>
+
+      <div>
+        <p>Reading Time</p>
+        <strong>{stats.readingTime.toFixed(2)} min</strong>
+      </div>
+    </div>
+  );
+};
